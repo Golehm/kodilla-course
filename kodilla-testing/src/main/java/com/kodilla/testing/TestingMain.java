31e24742c5ed78daad92f5d271d7a7cac2eaea9f
Golehm <grzegorz.gorski@yahoo.pl>
2018-04-18 16:40:21 +0200

    Zadanie 6.1

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing;
+
+public class TestingMain {
+    public static void main (String[] argv){
+
+        System.out.println("Modul 6. Wprowadzenie do testowania oprogramowania");
+
+    }
+}
