0e31cc20b6e619e762decc26b89684ba72b3ce6c
Golehm <grzegorz.gorski@yahoo.pl>
2018-04-19 20:31:42 +0200

    collection

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,7 +6,7 @@ import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String[] args){
 
-        Calculator calculator = new Calculator();
+    /*    Calculator calculator = new Calculator();
         System.out.println("Adding 7 " + "and 13 gives " + calculator.add(7,13));
         System.out.println("Subtracting 13 " + "from 7 gives " + calculator.subtract(7,13));
         SimpleUser simpleUser = new SimpleUser("theForumUser");
@@ -19,6 +19,6 @@ public class TestingMain {
             System.out.println("Error!");
         }
         // tu umieść kolejny test ->
-        System.out.println("Test - pierwszy test jednostkowy:");
+        System.out.println("Test - pierwszy test jednostkowy:");    */
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,55 @@
+package com.kodilla.testing.collection;
+
+import java.util.*;
+import com.kodilla.testing.collection.OddNumbersExterminator;
+import org.junit.*;
+
+public class CollectionTestSuite {
+    @Before
+    public void before(){
+        System.out.println("Test Case: begin");
+    }
+    @After
+    public void after(){
+        System.out.println("Test Case: end");
+    }
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Test Suite: begin");
+    }
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Test Suite: end");
+    }
+    @Test
+    public void testOddNumbersExterminatorEmptyListestCaseUsername(){
+        //Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        ArrayList<Integer> emptyNumbers = new ArrayList<Integer>();
+        //When
+        oddNumbersExterminator.exterminate(emptyNumbers);
+        //Then
+            System.out.println("Exterminate with empty list");
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList(){
+        //Given
+        OddNumbersExterminator oddNumbersExterminatorFull = new OddNumbersExterminator();
+        ArrayList<Integer> fullNumbers = new ArrayList<Integer>();
+        ArrayList<Integer> resultNumbers = new ArrayList<Integer>();
+        fullNumbers.add(0);
+        fullNumbers.add(1);
+        fullNumbers.add(2);
+        fullNumbers.add(3);
+        fullNumbers.add(4);
+        fullNumbers.add(5);
+        fullNumbers.add(6);
+        //When
+        resultNumbers = oddNumbersExterminatorFull.exterminate(fullNumbers);
+
+        //Then
+        System.out.println("Testing " + resultNumbers.get(0));
+        System.out.println("Testing " + resultNumbers.get(1));
+        System.out.println("Testing " + resultNumbers.get(2));
+    }
+}
