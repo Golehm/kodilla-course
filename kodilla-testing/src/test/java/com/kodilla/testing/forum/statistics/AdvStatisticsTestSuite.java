b6a0c508211c443a84d43a3280bd5405f2d9ce61
Golehm <grzegorz.gorski@yahoo.pl>
2018-04-25 19:58:22 +0200

    AdvStatistics test na 3

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/AdvStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/AdvStatisticsTestSuite.java
@@ -11,6 +11,7 @@ import static org.mockito.Mockito.mock;
 
 public class AdvStatisticsTestSuite {
     public void tester(int users, int posts, int comments){
+        //Given
         Statistics statisticsMock = mock(Statistics.class);
         List<String> usersNamesList = new ArrayList<String>();
         for(int i = 0; i < users; i++) {
@@ -19,25 +20,32 @@ public class AdvStatisticsTestSuite {
         when(statisticsMock.usersNames()).thenReturn(usersNamesList);
         when(statisticsMock.postsCount()).thenReturn(posts);
         when(statisticsMock.commentsCount()).thenReturn(comments);
-
         AdvStatistics advStat = new AdvStatistics();
         advStat.calculateAdvStatistics(statisticsMock);
+        //When
         int usersStat = advStat.numberOfUsers;
         int postsStat = advStat.numberOfPosts;
         int commentsStat = advStat.numberOfCommnets;
+        //Then
         Assert.assertEquals(users, usersStat);
         Assert.assertEquals(posts, postsStat);
         Assert.assertEquals(comments, commentsStat);
         advStat.showStatistics();
     }
     @Test
-    public void testCalculateAdvStatistics(){
-        tester(4,5,12);
-        tester(4,0,12);//zero posts
-        tester(4,1000,12);//1000 posts
-        tester(4,5,0);//zero comments
-        tester(4,6,3);//comments < posts
-        tester(4,3,7);//comments > posts
+    public void testCalculateAdvStatisticsPosts() {
+        tester(4, 5, 12);
+        tester(4, 0, 12);//zero posts
+        tester(4, 1000, 12);//1000 posts
+    }
+    @Test
+    public void  testCalculateAdvStatisticsComments() {
+        tester(4, 5, 0);//zero comments
+        tester(4, 6, 3);//comments < posts
+        tester(4, 3, 7);//comments > posts
+    }
+    @Test
+    public void testCalculateAdvStatisticsUsers(){
         tester(0,25,142);//0 users
         tester(100,50,126);//100 users
     }
