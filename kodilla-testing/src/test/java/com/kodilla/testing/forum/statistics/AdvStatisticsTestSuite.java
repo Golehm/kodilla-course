fedb6d11d554bf9411f527ab02cae73980eac6a2
Golehm <grzegorz.gorski@yahoo.pl>
2018-04-25 10:56:06 +0200

    poprawione statistics

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/AdvStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/AdvStatisticsTestSuite.java
@@ -10,24 +10,35 @@ import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.mock;
 
 public class AdvStatisticsTestSuite {
-    @Test
-    public void testCalculateAdvStatistics(){
+    public void tester(int users, int posts, int comments){
         Statistics statisticsMock = mock(Statistics.class);
-
-        List<String> usersNamesList = Arrays.asList("Ben Hur","Tali Ban","Al Kaida","Cent Nar");
-
+        List<String> usersNamesList = new ArrayList<String>();
+        for(int i = 0; i < users; i++) {
+            usersNamesList.add("Ben Hur");
+        }
         when(statisticsMock.usersNames()).thenReturn(usersNamesList);
-        when(statisticsMock.postsCount()).thenReturn(5);
-        when(statisticsMock.commentsCount()).thenReturn(12);
+        when(statisticsMock.postsCount()).thenReturn(posts);
+        when(statisticsMock.commentsCount()).thenReturn(comments);
 
         AdvStatistics advStat = new AdvStatistics();
         advStat.calculateAdvStatistics(statisticsMock);
-        int users = advStat.numberOfUsers;
-        int posts = advStat.numberOfPosts;
-        int comments = advStat.numberOfCommnets;
-        Assert.assertEquals(4, users);
-        Assert.assertEquals(5, posts);
-        Assert.assertEquals(12, comments);
+        int usersStat = advStat.numberOfUsers;
+        int postsStat = advStat.numberOfPosts;
+        int commentsStat = advStat.numberOfCommnets;
+        Assert.assertEquals(users, usersStat);
+        Assert.assertEquals(posts, postsStat);
+        Assert.assertEquals(comments, commentsStat);
         advStat.showStatistics();
+    }
+    @Test
+    public void testCalculateAdvStatistics(){
+        tester(4,5,12);
+        tester(4,0,12);//zero posts
+        tester(4,1000,12);//1000 posts
+        tester(4,5,0);//zero comments
+        tester(4,6,3);//comments < posts
+        tester(4,3,7);//comments > posts
+        tester(0,25,142);//0 users
+        tester(100,50,126);//100 users
     }
 }
