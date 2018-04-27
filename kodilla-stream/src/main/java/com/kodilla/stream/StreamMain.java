f295101640a21477309b710486ed57e745fce84e
Golehm <grzegorz.gorski@yahoo.pl>
2018-04-27 20:08:35 +0200

    folders out and build'

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,22 +1,10 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.person.People;
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier greatPoet = new PoemBeautifier();
-        greatPoet.beautify( "TIGER, tiger, burning bright", "@@@",
-                (poem, beautifier) -> beautifier + " " + poem + " " + beautifier);
-        greatPoet.beautify( "In the forests of the night,", "@@@",
-                (poem, beautifier) -> beautifier + " " + poem.toUpperCase() + " " + beautifier);
-        greatPoet.beautify( "What immortal hand or eye", "@@@",
-                (poem, beautifier) -> beautifier + " " + poem.toLowerCase() + " " + beautifier);
-        greatPoet.beautify( "Could frame thy fearful symmetry?", "@@@",
-                (poem, beautifier) -> beautifier + " " + poem.substring(0,24) + "..." + " " + beautifier);
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        People.getList().stream()
+                .forEach(System.out::println);
     }
-
 }
