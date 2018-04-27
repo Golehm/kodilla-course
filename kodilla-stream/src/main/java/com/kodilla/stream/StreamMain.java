e8c7c3f485124b1bf05a3b26ef4fde091dd7cce6
Golehm <grzegorz.gorski@yahoo.pl>
2018-04-27 16:45:11 +0200

    text beauty

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,35 +1,18 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.reference.FunctionalCalculator;
-//import com.kodilla.stream.lambda.Executor;
-//import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.beautifier.PoemBeautifier;
 
 public class StreamMain {
     public static void main(String[] args) {
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
-        System.out.println("Calculating expressions with lambdas");
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        PoemBeautifier greatPoet = new PoemBeautifier();
+        greatPoet.beautify( "TIGER, tiger, burning bright", "@@@",
+                (poem, beautifier) -> beautifier + " " + poem + " " + beautifier);
+        greatPoet.beautify( "In the forests of the night,", "@@@",
+                (poem, beautifier) -> beautifier + " " + poem.toUpperCase() + " " + beautifier);
+        greatPoet.beautify( "What immortal hand or eye", "@@@",
+                (poem, beautifier) -> beautifier + " " + poem.toLowerCase() + " " + beautifier);
+        greatPoet.beautify( "Could frame thy fearful symmetry?", "@@@",
+                (poem, beautifier) -> beautifier + " " + poem.substring(0,24) + "..." + " " + beautifier);
     }
-        //Processor processor = new Processor();
-        //Executor codeToExecute = () -> System.out.println("This is an example text.");
-        //processor.execute(codeToExecute);
-        //processor.execute(() -> System.out.println("This is an example text."));
 
-       // ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
-       // expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-      //  expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-      //  expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-       // expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -1,7 +1,8 @@
 package com.kodilla.stream.beautifier;
 
 public class PoemBeautifier {
-    public String beautify(String myPoem){
-        return myPoem;
+    public void beautify(String myPoem, String myBeautifier, PoemDecorator poemDecorator) {
+        String string = poemDecorator.decorate(myPoem, myBeautifier);
+        System.out.println(string);
     }
 }
