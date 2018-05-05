8bb30031ccd1196946b8493a3296a043421b43b6
Golehm <grzegorz.gorski@yahoo.pl>
2018-05-05 17:15:00 +0200

    bez zbednych list

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreMain.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreMain.java
@@ -1,19 +1,17 @@
 package com.kodilla.good.patterns.challenges;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class MovieStoreMain {
     public static void main(String[] argv){
+
         MovieStore movieStore = new MovieStore();
 
-        List<String> listOfMovies = movieStore.getMovies().entrySet().stream()
+                movieStore.getMovies().entrySet().stream()
                 .flatMap(movies -> movies.getValue().stream())
-                .collect(Collectors.toList());
-
-        List<String> listToPrint = listOfMovies.stream().map(movie -> movie + "!").collect(Collectors.toList());
-
-        listToPrint.forEach(System.out::print);
+                .collect(Collectors.toList()).stream().
+                        map(movie -> movie + "!").
+                        collect(Collectors.toList())
+                        .forEach(System.out::print);
     }
 }
