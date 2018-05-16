4b57a2a2046c1ae3b3286ceb81a95de2f2f8ecbb
Golehm <grzegorz.gorski@yahoo.pl>
2018-05-16 11:58:36 +0200

    Zadanie 10.2

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,5 +1,32 @@
 package com.kodilla.spring.calculator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class Calculator {
-    //Display display
+
+    @Autowired
+    private Display display;
+
+    double add(double a, double b) {
+        double c = a + b ;
+        display.displayValue(c);
+        return c ;
+    }
+    double sub(double a, double b) {
+        double c = a - b ;
+        display.displayValue(c);
+        return c ;
+    }
+    double mul(double a, double b) {
+        double c = a * b ;
+        display.displayValue(c);
+        return c ;
+    }
+    double div(double a, double b) throws ArithmeticException {
+        double c = a / b;
+        display.displayValue(c);
+        return c;
+    }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,4 +1,24 @@
 package com.kodilla.spring.calculator;
 
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.swing.*;
+
+@SpringBootTest
 public class CalculatorTestSuite {
+    @Test
+    public void testCalculations() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        calculator.add(1,3);
+        calculator.sub(2,2);
+        calculator.mul(3,1);
+        calculator.div(4,0);
+    }
 }
