6b4ac594f3e9d2f067e6d8ca0a90ab990b38863e
Golehm <grzegorz.gorski@yahoo.pl>
2018-08-12 21:26:21 +0200

    23.3 excercise

--- kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/crudapp/CrudAppTestingApp.java
@@ -10,7 +10,7 @@ public class CrudAppTestingApp {
 
     public final static String XPATH_INPUT = "//html/body/main/section/form/fieldset/input";
     public final static String XPATH_TEXTAREA = "//html/body/main/section/form/fieldset[2]/textarea";
-    public final static String XPATH_WAIT_FOR = "//SELECT[1]";
+    public final static String XPATH_WAIT_FOR = "//select[1]";
     public final static String XPATH_SELECT = "//div[contains(@class, \"tasks-container\")]/form/div/fieldset/select[1]";
 
     public static void main(String[] args) {
--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -1,7 +1,21 @@
 package com.kodilla.testing2.facebook;
 
-public class FacebookTestingApp {
-    public static void main(String[] args) {
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
+public class FacebookTestingApp {
+    public final static String XPATH_WAIT_FOR = "//select[1]";
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        driver.get("https://www.facebook.com");
+        while (!driver.findElement(By.xpath(XPATH_WAIT_FOR)).isDisplayed());
+        Select birthDay = new Select(driver.findElement(By.name("birthday_day")));
+        birthDay.selectByVisibleText("1");
+        Select birthMonth = new Select(driver.findElement(By.name("birthday_month")));
+        birthMonth.selectByVisibleText("lut");
+        Select birthYear = new Select(driver.findElement(By.name("birthday_year")));
+        birthYear.selectByVisibleText("1999");
     }
 }
